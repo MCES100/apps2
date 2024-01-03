3c48a46fc84f00de4edb9eb6bd137f403b509f4d
MCES10 <138902870+MCES10@users.noreply.github.com>
2024-01-03 20:58:20 +0000

    main

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,8 @@
 <template>
-    <div>index page</div>
-    <base-button type="default">Default</base-button>
+    <div></div>
+    <center>
+    <h1 class="font-extrabold">MCES10 Software</h1>
+  </center>
 </template>
 <style>
 
@@ -27,5 +29,12 @@ body {
   background: rgb(0,0,0);
   background: linear-gradient(90deg, rgba(0,0,0,1) 0%, rgba(2,0,36,1) 66%, rgba(0,85,244,1) 100%);
 }
+
+h1 {
+  padding-top: 5rem;
+  font-size: 3.75rem;
+line-height: 1rem;
+}
+
 
     </style>
