e1ca24a5f7c31072e725aac46b518fd04704773b
MCES10 <138902870+MCES10@users.noreply.github.com>
2024-01-03 14:07:01 +0000

    main

--- app.vue
+++ app.vue
@@ -1,5 +1,15 @@
 <template>
   <div>
+    <Navbar />
     <p>Hello World</p>
   </div>
 </template>
+<script>
+import Navbar from '~/components/Navbar.vue';
+
+export default {
+  components: {
+    Navbar,
+  },
+};
+</script>
---
+++ components/Navbar.vue
@@ -0,0 +1,48 @@
+<!-- components/Navbar.vue -->
+
+<template>
+    <nav>
+      <ul>
+        <li>
+          <nuxt-link to="/">Home</nuxt-link>
+        </li>
+        <li>
+          <nuxt-link to="/about">About</nuxt-link>
+        </li>
+        <li>
+          <nuxt-link to="/contact">Contact</nuxt-link>
+        </li>
+      </ul>
+    </nav>
+  </template>
+  
+  <script>
+  export default {
+    name: 'Navbar',
+  };
+  </script>
+  
+  <style scoped>
+  /* Add your styling here */
+  nav {
+    background-color: #333;
+    padding: 1rem;
+  }
+  
+  ul {
+    list-style: none;
+    display: flex;
+    justify-content: space-around;
+  }
+  
+  li {
+    margin-right: 10px;
+  }
+  
+  a {
+    text-decoration: none;
+    color: white;
+    font-weight: bold;
+  }
+  </style>
+  
