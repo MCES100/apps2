03804fbfdc3fbf82648d90d1f4b94e189b5a18b0
MCES10 <138902870+MCES10@users.noreply.github.com>
2024-01-03 20:39:38 +0000

    main

--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,6 @@
 <template>
     <div>index page</div>
-    <button class="btn">
-      <span class="btn-content">
-        <span class="btn-text">Click me</span>
-        <span class="subtitle">Subtitle here</span>
-        <span class="arrow-icon">&#10148;</span>
-      </span>
-    </button>
+    
 </template>
 <style>
 
@@ -33,37 +27,5 @@ body {
   background: rgb(0,0,0);
   background: linear-gradient(90deg, rgba(0,0,0,1) 0%, rgba(2,0,36,1) 66%, rgba(0,85,244,1) 100%);
 }
-.btn {
-  @apply relative inline-block px-6 py-4 text-lg font-semibold rounded-full bg-opacity-25 bg-white text-white hover:bg-opacity-50 hover:bg-white border border-white border-opacity-25 transition duration-300 ease-in-out;
-  backdrop-filter: blur(10px);
-  overflow: hidden; /* Ensure overflow is hidden to clip arrow animation */
-}
 
-.btn-content {
-  @apply relative;
-}
-
-.btn-text {
-  @apply block mb-1; /* Display as block and add margin-bottom for separation */
-}
-
-.subtitle {
-  @apply text-sm text-gray-300; /* Adjust the font size and color as needed */
-}
-
-.arrow-icon {
-  @apply absolute bottom-0 right-0 transform translate-x-full translate-y-full text-lg opacity-0 transition-transform duration-300 ease-in-out;
-}
-
-.btn:hover .arrow-icon {
-  @apply opacity-100 transform translate-x-0 translate-y-0;
-}
-/*
-
-background: linear-gradient(
-      to bottom,
-      transparent,
-      rgb(var(--background-end-rgb))
-    )
-    rgb(var(--background-start-rgb)); */
     </style>
