f33d4501e00296edab6e0257d29ffcc0f6d566c1
MCES10 <138902870+MCES10@users.noreply.github.com>
2024-01-03 13:10:57 +0000

    main

--- src/my-endpoint.get.js
+++
@@ -1,8 +0,0 @@
-export default defineEventHandler(({ context }) => {
-    // the type `KVNamespace` comes from the @cloudflare/workers-types package
-    const MY_KV: KVNamespace = context.cloudflare.env.MY_KV;
-  
-    return {
-      // ...
-    };
-  });
