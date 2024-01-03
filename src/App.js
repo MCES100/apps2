3fa138a09531e3d8035c7866d6dbbdbf2e395f16
MCES10 <138902870+MCES10@users.noreply.github.com>
2024-01-03 13:38:00 +0000

    main

--- src/App.js
+++ src/App.js
@@ -1,95 +1,15 @@
-import logo from './logo.svg';
+import logo from './Logo.png';
 import './App.css';
-import './globals.css';
+import './global.css'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-      <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 `}
-    >
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-         
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute"> 
-      
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://mces10-software.github.io/apps/404.html"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            RickyAI{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Try our AI generative coming soon..
-          </p>
-        </a>
-
-        <a
-          href="https://mces10-software.github.io/apps/downloadlogin.html"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Web Development Tutorials{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn how to code websites 
-          </p>
-        </a>
-
-        <a
-          href="https://mces10-software.github.io/apps/downloadlogin.html"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            To-List{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-          The perfect way to show your to-dos and organise them 
-          </p>
-        </a>
-
-        <a
-          href="https://mces10-software.github.io/apps/accountlogin.html"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            MCES10 Software 24"{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Wait for a event that will present newer features.
-          </p>
-        </a>
-      </div>
-    </main>
-       
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>        
+        MCES10 Software
+        </p>
       </header>
     </div>
   );
