75f94578345258511a55edfd600d5992244eec08
MCES10 <138902870+MCES10@users.noreply.github.com>
2024-01-04 22:26:45 +0000

    main

--- pages/index.vue
+++ pages/index.vue
@@ -29,9 +29,9 @@
       </a>
 
       <a
-        href="#"
+        href="/wdt"
         class="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-        target="_blank"
+        target="_parent"
         rel="noopener noreferrer"
       >
         <h2 class="mb-3 text-2xl font-semibold">
---
+++ pages/wdt.vue
@@ -0,0 +1,33 @@
+<template>
+    <center>
+    <h1>Welcome To </h1>
+    <h1>Web Development </h1>
+    <h1>Tutorials</h1>
+    <img src="/img/WDT.png" alt="MCES10 Software Logo" width="100px" height="100" />
+    <h2 class="mb-2 text-lg font-semibold text-gray-900 dark:text-white">App Contents</h2>
+    <ul class="max-w-md space-y-1 text-gray-500 list-disc list-inside dark:text-gray-400" style="padding: 2rem">
+    <li>
+       HTML, CSS and JavaScript (HTML, CSS Free JS in Pro)
+    </li>
+    <li>
+        Nuxt.js, Next.js and React courses are in the works (Pro Only)
+    </li>
+    <li>
+        Firebase Auth.
+    </li>
+    <li>
+        Code Editior Side by side with the course if needed on iPadOS
+    </li>
+    
+</ul>
+</center>
+</template>
+<style>
+h1{
+    padding-bottom: 2rem;
+}
+
+</style>
+  
+ 
+  
