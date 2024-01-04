17a73e6f31af7c4bae8629e05d6a096a78d18a22
MCES10 <138902870+MCES10@users.noreply.github.com>
2024-01-04 09:25:36 +0000

    main

--- pages/index.vue
+++ pages/index.vue
@@ -2,8 +2,20 @@
     <div></div>
     <center>
     <h1 class="font-extrabold">MCES10 Software</h1>
+    <nicepaddingsides>
+    <table>
+      <tr>
+        <td><h2>Welcome to World of Software. Get ready for our app releases and MCES10 Software 24".</h2></td>
+        <td><UButton to="#" target="_blank" size="xl" color="blue">Sign Up</UButton></td>
+      </tr>
+    </table>
+  </nicepaddingsides>
     
-    <h2>Welcome to World of Software. Get ready for our app releases and MCES10 Software 24".</h2>
+    <h2>Try our Beta Products</h2>
+    <UButton to="#" target="_blank" size="xl" color="blue" variant="ghost" >Ricky AI</UButton>
+    <UButton to="#" target="_blank" size="xl" color="blue" variant="ghost" >Web Dev Tut</UButton>
+    <UButton to="#" target="_blank"  size="xl" color="blue" variant="ghost">To-list</UButton>
+    <UButton to="#" target="_blank"  size="xl" color="blue" variant="ghost">Others</UButton>
   </center>
 </template>
 <style>
@@ -37,6 +49,10 @@ h1 {
   padding-bottom: 5rem;
   font-size: 3.75rem;
 line-height: 1rem;
+}
+.nicepaddingsides {
+  padding-left: 5rem;
+  padding-right: 5rem;
 }
 
 
